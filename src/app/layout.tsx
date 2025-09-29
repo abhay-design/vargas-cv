29b788757d1b77505ce1d4cce1b56b28506b8814
Abhay <abhay@cleardigital.com>
2025-09-29 19:16:04 +0530

    adding simple layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -24,10 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.variable}  antialiased overflow-y-scroll body`}>
+        <div className="max-w-[780px] p-4 md:p-10 mx-auto bg-background min-h-screen">
+          {children}
+        </div>
       </body>
     </html>
   );
