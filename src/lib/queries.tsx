399b8c4795cc6bf60b6f3e2f3ddec44326c1c3d1
Abhay <abhay@cleardigital.com>
2025-09-30 19:21:57 +0530

    fixed cache issue

--- src/lib/queries.tsx
+++ src/lib/queries.tsx
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL;
 import { category, Post } from "./types";
-const revelidateTime: number = 86400;
+const revelidateTime: number = 30;
 
 export async function getCategories(): Promise<category[]> {
   if (!baseUrl) {
@@ -22,21 +22,30 @@ export async function getAllposts(
     per_page: perPage.toString(),
     page: pageNumber.toString(),
     search: searchTerm,
+    _embed: "true", // Add this to include featured media
+    timestamp: Date.now().toString(), // Cache buster
   });
 
   if (categories !== 0) {
     params.set("categories", categories.toString());
   }
 
-  console.log(`${baseUrl}/wp-json/wp/v2/posts?${params}`);
+  // console.log(`${baseUrl}/wp-json/wp/v2/posts?${params}`);
   const response = await fetch(
     `${baseUrl}/wp-json/wp/v2/posts?${params.toString()}`,
     {
       next: {
         revalidate: revelidateTime,
+        // Or use tags for on-demand revalidation
+        tags: ["posts-collection"],
       },
     }
   );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   const posts = await response.json();
   const totalPages = parseInt(response.headers.get("X-WP-TotalPages") ?? "1");
   return { posts, totalPages };
