4d7f0b97328557d1e846c5b001bc35c9a30f2985
Abhay <abhay@cleardigital.com>
2025-09-30 14:56:40 +0530

    fixed build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Socialicons from "@/components/Social-icons";
 import { Categories } from "@/components/Categories";
 import { getCategories } from "@/lib/queries";
 
+console.log("API URL:", process.env.NEXT_PUBLIC_WORDPRESS_URL);
 export default async function Home() {
   const categories = await getCategories();
   return (
