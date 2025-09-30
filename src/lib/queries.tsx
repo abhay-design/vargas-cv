0c0167a35bb766b406d4db99337b09010ecbb302
Abhay <abhay@cleardigital.com>
2025-09-30 14:45:14 +0530

    fixed build

--- src/lib/queries.tsx
+++ src/lib/queries.tsx
@@ -1,7 +1,11 @@
-const baseUrl = process.env.WORDPRESS_URL;
+const baseUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL;
 import { category } from "./types";
 
 export async function getCategories(): Promise<category[]> {
+  if (!baseUrl) {
+    throw new Error("Missing NEXT_PUBLIC_WORDPRESS_URL");
+  }
+
   const response = await fetch(`${baseUrl}/wp-json/wp/v2/categories`);
   const data = await response.json();
   return data;
